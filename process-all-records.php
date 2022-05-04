949408bd76fd22727d9641c467554bf097a4d4f5
Mark McEver <mark@fastmail.net>
2022-05-04 15:24:27 -0500

    Added script to process all records

---
+++ process-all-records.php
@@ -0,0 +1,70 @@
+<?php
+if(SUPER_USER !== '1'){
+    die("You're not allowed to view this page!");
+}
+
+$recordIdFieldName = json_decode(\REDCap::getDataDictionary($_GET['pid'], 'json'), true)[0]['field_name'];
+
+$fieldNames = [
+    $recordIdFieldName
+];
+
+$subSettings = $module->getSubSettings('concatenated-fields');
+foreach($subSettings as $field_data) {
+    $fieldNames[] = $field_data['destination'];
+    foreach ($field_data['source'] as $src) {
+        $fieldNames[] = $src;
+    }    
+}
+
+$data = json_decode(REDCap::getData([
+    'return_format' => 'json',
+    'fields' => $fieldNames,
+]), true);
+
+$recordsToSave = [];
+foreach($data as $record){
+    $recordId = $record[$recordIdFieldName];
+    $recordToSave = [];
+    foreach($subSettings as $field_data) {
+        $destField = $field_data['destination'];
+        $srcFields = $field_data['source'];
+        if (!is_array($srcFields)) {
+            $srcFields = array($srcFields);
+        }
+        $spaces = $field_data['spaces'];
+        $space = "";
+        if ($spaces) {
+            $space = " ";
+        }
+
+        
+        if ($destField) {
+            $expected = '';
+
+            foreach ($srcFields as $src) {
+                if($field_data['spaces'] && !empty($expected)){
+                    $expected .= " ";
+                }
+
+                $expected .= $record[$src];
+            }
+
+            $actual = $record[$destField];
+
+            if($expected !== $actual){
+                echo "$recordId, $expected, $actual<br>";
+                $recordToSave[$recordIdFieldName] = $recordId;
+                $recordToSave[$destField] = $expected;
+            }
+        }
+    }
+
+    if(!empty($recordToSave)){
+        $recordsToSave[] = $recordToSave;
+    }
+}
+
+echo "<pre>";
+var_dump($recordsToSave);
+echo "</pre>";
